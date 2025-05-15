be0e79bc44f5d51ff2e557ad6a006b12d847dbfe
katerus16 <sherryyy@yandex.ru>
2025-05-16 00:51:44 +0300

    rollbar

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,16 +8,25 @@ import store from './slices/index.js';
 import i18nextru from './i18next.js'
 import { I18nextProvider } from 'react-i18next';
 import { ToastContainer } from 'react-toastify';
+import { ErrorBoundary, Provider as RollbarProvider } from '@rollbar/react';
 
 const app = async() => {
+  const rollbarConfig = {
+    accessToken: process.env.REACT_APP_ROLLBAR_TOKEN, 
+    environment: 'production',
+  };
 const i18n = await i18nextru();
 createRoot(document.getElementById('chat')).render(
   <StrictMode>
     <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <App />
-        <ToastContainer pauseOnFocusLoss={false} position="top-right" />
-      </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <Provider store={store}>
+            <App />
+            <ToastContainer pauseOnFocusLoss={false} position="top-right" />
+          </Provider>
+        </ErrorBoundary>
+      </RollbarProvider>
     </I18nextProvider>
   </StrictMode>,
 )}
