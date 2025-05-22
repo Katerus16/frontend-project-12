bdc2027912dcf52a2a8f7221cecf2140df1cbd8c
katerus16 <sherryyy@yandex.ru>
2025-05-22 22:36:03 +0300

    eslint

--- frontend/src/Init.jsx
+++ frontend/src/Init.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore(reducer)
-  const Init = async () => {
+const Init = async () => {
   const socket = io()
   const rollbarConfig = {
   // eslint-disable-next-line
